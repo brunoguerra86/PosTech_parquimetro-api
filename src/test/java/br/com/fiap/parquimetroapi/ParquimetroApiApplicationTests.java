412498f5ff8c5647027ba5ad51a2189eef7786eb
brunoguerra <brunoguerra@hotmail.com>
2023-12-23 16:51:12 -0300

    first commit

---
+++ src/test/java/br/com/fiap/parquimetroapi/ParquimetroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.parquimetroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParquimetroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
